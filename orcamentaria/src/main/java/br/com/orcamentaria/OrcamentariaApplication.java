b5a9327286ef5f3239519ebf741a22e3cc277910
Leo Laia <leonardolribo@gmail.com>
2024-02-12 11:15:48 -0300

    Initial Commit

---
+++ orcamentaria/src/main/java/br/com/orcamentaria/OrcamentariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.orcamentaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentariaApplication.class, args);
+	}
+
+}
